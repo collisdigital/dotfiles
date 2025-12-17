a046252bb534668e6a9badebfba0212df92e884b
Chris Collis <collisdigital@users.noreply.github.com>
2025-12-17 14:17:21 +0000

    Use absolute path for symlinking .gitconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "Listing current directory..."
 ls -la .
 
 echo "Symlinking .gitconfig..."
-ln -v -s ./.gitconfig ~/.gitconfig
+ln -v -s "$PWD/.gitconfig" "$HOME/.gitconfig"
 
 echo "Installing Gemini CLI..."
 npm install -g @google/gemini-cli
